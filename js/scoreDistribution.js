ce3076621946ab9f9003dcbce5a812bdf2213d4a
Huyen Nguyen <huyen.nguyen@ttu.edu>
2020-04-21 17:05:09 +0700

    Collect data and crop images

---
+++ js/scoreDistribution.js
@@ -0,0 +1,22 @@
+main = "#main"
+
+// set the dimensions and margins of the graph
+let margin = {top: 10, right: 30, bottom: 30, left: 40},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select(main)
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+
+d3.csv("data/data.csv", function (error, data) {
+    if (error) throw error;
+
+    console.log(data);
+})
