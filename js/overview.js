16eff03f024422f22366b59c3263a029eb59138a
Huyen Nguyen <huyen.nguyen@ttu.edu>
2020-04-24 00:38:40 +0700

    Update zoomed images

---
+++ js/overview.js
@@ -0,0 +1,7 @@
+d3.csv("data/newData.csv", function (error, data) {
+    if (error) throw error;
+
+    console.log(data);
+
+
+})
--- js/scoreDistribution.js
+++ js/scoreDistribution.js
@@ -1,49 +1,56 @@
 main = "#main"
 
-// set the dimensions and margins of the graph
-let margin = {top: 10, right: 30, bottom: 30, left: 40},
-    width = 2500 - margin.left - margin.right,
-    height = 800 - margin.top - margin.bottom;
-
-let zoomPanelMargin = {top: 20, right: 20, bottom: 20, left: 20},
-    zoomPanelWidth = 400 - zoomPanelMargin.left - zoomPanelMargin.right,
-    zoomPanelHeight =  400 - zoomPanelMargin.top - zoomPanelMargin.bottom;
-
-// append the svg object to the body of the page
-var svg = d3.select(main)
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .attr("class", "imageDist")
-    .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+let thumbImgWidth = 20, thumbImgHeight = 20
+let biggerImgWidth = 450, biggerImgHeight = 360
 
 
-d3.csv("data/newData.csv", function (error, data) {
+
+d3.csv("data/newData2.csv", function (error, data) {
     if (error) throw error;
 
     console.log(data);
-    let topic = "birdCall"
+    let topic = "eyeball"
 
     console.log(data.filter(d => d.Label === topic))
 
+    // set the dimensions and margins of the graph
+    let margin = {top: 10, right: 30, bottom: 30, left: 160},
+        width = 1650 - margin.left - margin.right,
+        height = 800 - margin.top - margin.bottom;
+
+    let zoomPanelMargin = {top: 20, right: 20, bottom: 20, left: 20},
+        zoomPanelWidth = 350 - zoomPanelMargin.left - zoomPanelMargin.right,
+        zoomPanelHeight =  350 - zoomPanelMargin.top - zoomPanelMargin.bottom;
+
+
+    // append the svg object to the body of the page
+    var svg = d3.select(main)
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .attr("class", "imageDist")
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
     //    x-axis
     let x = d3.scaleLinear()
-        .domain([0, 1])
-        .range([0, width])
+        .domain([0.25, 1])
 
-    svg.append("g")
-        .attr("transform", "translate(0, " + height + ")")
-        .call(d3.axisBottom(x))
 
     // set the parameters for histogram
     histogram = d3.histogram()
         .value(d => d.Score)
         .domain(x.domain())
-        .thresholds(x.ticks(120))
+        .thresholds(x.ticks(100))
 
     bins = histogram(data.filter(d => d.Label === topic).sort((a ,b) => parseFloat(b.Score) - parseFloat(a.Score)))
+
+    x.range([0, width])
+
+    svg.append("g")
+        .attr("transform", "translate(0, " + height + ")")
+        .call(d3.axisBottom(x))
 
     // y-axis
     let y = d3.scaleLinear()
@@ -51,9 +58,10 @@ d3.csv("data/newData.csv", function (error, data) {
 
     y.domain([0, d3.max(bins, d => d.length)])
 
-    let yAxisHandleForUpdate = svg.append("g")
+    svg.append("g")
         .call(d3.axisLeft(y))
 
+    let barWidth = x(bins[1].x1) - x(bins[1].x0) -3
     // append rectangles
     svg.selectAll(null)
         .data(bins)
@@ -65,10 +73,12 @@ d3.csv("data/newData.csv", function (error, data) {
         .attr("height", d => height - y(d.length))
         .style("fill", "#e4f2f7")
 
-    // add sticky div
+    // add div
     d3.select(main).append("div")
-        .attr("class", "sticky box")
+        .attr("class", "box")
         .attr("id", "zoomPanel")
+        .style("opacity", 0)
+
 
     let zoomPanel = d3.select("#zoomPanel").append("svg")
         .attr("width", zoomPanelWidth + zoomPanelMargin.left + zoomPanelMargin.right)
@@ -78,10 +88,29 @@ d3.csv("data/newData.csv", function (error, data) {
             "translate(" + zoomPanelMargin.left + "," + zoomPanelMargin.top + ")");
 
     let imageZoomed = zoomPanel.append('image')
-        .attr("width", d => {
-            return zoomPanelWidth
-        })
+        .attr("width", zoomPanelWidth)
         .attr("height", zoomPanelHeight)
+
+    let textZoomed = d3.select("#zoomPanel")
+        .append("div")
+        // .style("margin-top", "20px")
+
+
+    // add div
+    d3.select(main).append("div")
+        .attr("id", "detailPanel")
+
+    // whole Image
+    let wholeImagePanel = d3.select("#detailPanel").append("svg")
+        .attr("width", biggerImgWidth + zoomPanelMargin.left + zoomPanelMargin.right)
+        .attr("height", biggerImgHeight + zoomPanelMargin.top + zoomPanelMargin.bottom)
+        .append("g")
+
+
+    let biggerImage = wholeImagePanel.append('image')
+        .attr("width", biggerImgWidth)
+        .attr("height", biggerImgHeight)
+        // .attr("y", "0px")
 
     let groups = svg.selectAll(null)
         .data(bins)
@@ -90,7 +119,6 @@ d3.csv("data/newData.csv", function (error, data) {
             .attr("transform", d => "translate(" + x(d.x0) + "," + y(d.length) + ")")
     ;
 
-
     let prevClick
     let rects = groups.selectAll(".thumbnail")
         .data(d => d)
@@ -98,19 +126,28 @@ d3.csv("data/newData.csv", function (error, data) {
         .append('image')
         .attr("class", "thumbnail")
         .attr('xlink:href', (d) =>  {
-            return "MC2-Image-Data/" + d.Person + "/" + d.Image + ".jpg"
+            return "MC2-Image-Data/" + d.Person + "/" + d.ID + ".jpg"
         })
         .attr("x", 1)
         .attr("y", (d, i) => height - y(i))
-        .attr("width", d => {
-            return 21
-        })
-        .attr("height", 21)
-        .on("click", function(d) {
+        .attr("width", barWidth)
+        .attr("height", barWidth)
+        .on("mouseover", function(d) {
             console.log("--------------------")
             console.log(d)
-            imageZoomed.attr('xlink:href', "MC2-Image-Data/" + d.Person + "/" + d.Image + ".jpg")
-            d3.select("#zoomPanel").classed("redBorder", true)
+
+            imageZoomed.attr('xlink:href', "MC2-Image-Data/" + d.Person + "/" + d.ID + ".jpg")
+
+            d3.select("#zoomPanel")
+                .style("opacity", 1)
+                .classed("redBorder", true)
+
+            biggerImage.attr('xlink:href', "MC2-Image-Data/" + d.Person + "/" + d.Image + "bbox.jpg")
+
+            textZoomed.html("<br>" + "Score: " + d.Score + "<br>" + " Label: " + d.Label +  "<br>" + " Owner: " + d.Person
+                + (parseInt(d.x) < 0 ? "<br><span style='color: red'> Cannot locate bounding box for this" +
+                    " object!</span>" : "")
+            )
 
             d3.select(this).classed("imgBorder", true)
             d3.select(prevClick).classed("imgBorder", false)
@@ -118,40 +155,11 @@ d3.csv("data/newData.csv", function (error, data) {
 
         })
 
-
-
-    var updateBars = function (data) {
-        // First update the y-axis domain to match data
-
-        y.domain([0, d3.max(bins, d => d.length)])
-        yAxisHandleForUpdate.call(d3.axisLeft(y))
-
-        let bars = svg.selectAll(".bar").data(data)
-
-        // // add bars for new data
-        // bars.enter()
-        //     .append("rect")
-        //     .attr("class", "bar")
-        //     .attr("x", 1)
-        //     .attr("transform", d => "translate(" + x(d.x0) + "," + y(d.length) + ")")
-        //     .attr("width", d => x(d.x1) > x (d.x0) ? x(d.x1) - x (d.x0) - 1: 0)
-        //     .attr("height", d => height - y(d.length))
-        //     .style("fill", "#1c6363")
-        //
-        // bars.transition().duration(400)
-        //     .attr("height", d => height - y(d.length))
-        //
-        // bars.exit().remove();
-
-    }
-
     // Handler for dropdown value change
     function dropdownChange() {
         let newTopic = d3.select(this).property('value')
-
-        let newData = histogram(data.filter(d => d.Label === newTopic))
         console.log(newTopic)
-        updateBars(newData)
+
     }
 
     let dropdown = d3.select(main)
@@ -167,8 +175,5 @@ d3.csv("data/newData.csv", function (error, data) {
         .text(function (d) {
             return d[0].toUpperCase() + d.slice(1,d.length); // capitalize 1st letter
         });
-
-    updateBars(bins);
-
 
 })
